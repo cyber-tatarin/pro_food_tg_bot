0414d86ff6e985d1ec3f54a6a7752bfe3c808880
cyber-tatarin <dmitriyseur@gmail.com>
2023-10-14 16:06:36 +0300

    small fixes

--- root/jobs/main.py
+++ root/jobs/main.py
@@ -59,7 +59,7 @@ def get_body_measures_job():
 
 # Schedule the job to run every day at 10 pm
 schedule.every().day.at("23:55").do(set_user_streak_to_0_if_was_not_active_today_job)
-schedule.every().saturday.at("15:54").do(get_body_measures_job)
+schedule.every().saturday.at("16:08").do(get_body_measures_job)
 
 
 async def run_schedule():
