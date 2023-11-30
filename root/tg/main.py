69856b27950b7891a7c69ad4522649720fa08c65
cyber-tatarin <dmitriyseur@gmail.com>
2023-12-01 01:31:58 +0300

    add onboarding

--- root/tg/main.py
+++ root/tg/main.py
@@ -68,8 +68,8 @@ class GetMeasuresState(StatesGroup):
 
 @dp.message(Command('start'))
 async def start(message: types.Message):
-    videoguide_file = FSInputFile(path='/media/video_2023-12-01_01-00-47.mp4')
-    await bot.send_video(message.from_user.id, videoguide_file, caption=texts.start_video_caption)
+    videoguide_file = FSInputFile(path='media/video_2023-12-01_01-00-47.mp4', filename='Onboarding.mp4')
+    await bot.send_video(message.from_user.id, video=videoguide_file, caption=texts.start_video_caption)
     await asyncio.sleep(10)
     await message.answer(texts.start_message, reply_markup=keyboards.get_ikb_to_get_user_start_data())
     
